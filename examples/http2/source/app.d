26f94dec80f941b2ac307e2063ec293b4d96eaf8
GallaFrancesco <francesco.galla3@gmail.com>
2019-05-13 13:19:54 +0200

    Example application w. HTTP/2

---
+++ examples/http2/source/app.d
@@ -0,0 +1,92 @@
+/* ==== Vibe.d HTTP/2 Webserver Example ==== */
+/* 	Supports both HTTP and HTTPS transport   */
+/*  Transparent (WIP: exposing settings) 	 */
+/* ========================================= */
+
+import vibe.http.server;
+import vibe.stream.tls;
+import vibe.http.internal.http2.http2 : http2Callback; // ALPN negotiation
+import vibe.core.core : runApplication;
+
+/* ==== declare two handlers (could use the same one) ==== */
+void handleReq(HTTPServerRequest req, HTTPServerResponse res)
+@safe {
+	if (res.httpVersion == HTTPVersion.HTTP_2)
+		res.writeBody("Hello, you connected to "~req.path~"! This response is sent through HTTP/2\n");
+	else
+		res.writeBody("Hello, World! You connected through HTTP/1, try using HTTP/2!\n");
+}
+
+void tlsHandleReq(HTTPServerRequest req, HTTPServerResponse res)
+@safe {
+	if (req.httpVersion == HTTPVersion.HTTP_2)
+		res.writeBody("Hello, you connected to "~req.path~"! This response is sent through HTTP/2 with TLS\n");
+	else
+		res.writeBody("Hello, World! You connected through HTTP/1 with TLS, try using HTTP/2!\n");
+}
+
+// sends a very big data frame
+void bigHandleReq(size_t DIM)(HTTPServerRequest req, HTTPServerResponse res)
+@trusted {
+	import vibe.utils.array : FixedAppender;
+	import std.range : iota;
+
+	FixedAppender!(immutable(char)[], DIM) appender;
+
+	if (req.path == "/") {
+		foreach(i; iota(1,DIM-4)) appender.put('1');
+		appender.put(['O','k','!', '\n']);
+		res.writeBody(appender.data);
+	}
+}
+
+void main()
+{
+	//import vibe.core.log;
+	//setLogLevel(LogLevel.trace);
+
+/* ==== cleartext HTTP/2 support (h2c) ==== */
+	auto settings = HTTPServerSettings();
+	settings.port = 8090;
+	settings.bindAddresses = ["127.0.0.1"];
+	listenHTTP!handleReq(settings);
+
+/* ==== cleartext HTTP/2 support (h2c) with a heavy DATA frame ==== */
+	auto bigSettings = HTTPServerSettings();
+	settings.port = 8092;
+	settings.bindAddresses = ["127.0.0.1"];
+	listenHTTP!(bigHandleReq!100000)(settings);
+
+/* ========== HTTPS (h2) support ========== */
+	HTTPServerSettings tlsSettings;
+	tlsSettings.port = 8091;
+	tlsSettings.bindAddresses = ["127.0.0.1"];
+
+	/// setup TLS context by using cert and key in example rootdir
+	tlsSettings.tlsContext = createTLSContext(TLSContextKind.server);
+	tlsSettings.tlsContext.useCertificateChainFile("server.crt");
+	tlsSettings.tlsContext.usePrivateKeyFile("server.key");
+
+	// set alpn callback to support HTTP/2 protocol negotiation
+	tlsSettings.tlsContext.alpnCallback(http2Callback);
+	listenHTTP!tlsHandleReq(tlsSettings);
+
+/* ========== HTTPS (h2) support with a heavy DATA frame ========== */
+	HTTPServerSettings bigTLSSettings;
+	bigTLSSettings.port = 8093;
+	bigTLSSettings.bindAddresses = ["127.0.0.1"];
+
+	/// setup TLS context by using cert and key in example rootdir
+	bigTLSSettings.tlsContext = createTLSContext(TLSContextKind.server);
+	bigTLSSettings.tlsContext.useCertificateChainFile("server.crt");
+	bigTLSSettings.tlsContext.usePrivateKeyFile("server.key");
+
+	// set alpn callback to support HTTP/2 protocol negotiation
+	bigTLSSettings.tlsContext.alpnCallback(http2Callback);
+	auto l = listenHTTP!(bigHandleReq!100000)(bigTLSSettings);
+	scope(exit) l.stopListening();
+
+/* ========== Run both `listenHTTP` handlers ========== */
+	// UNCOMMENT to run
+	runApplication();
+}
