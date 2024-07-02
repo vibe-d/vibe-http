128011a04682478d7f83461085ed00e802f8f7d6
Alexibu <alex@sunopti.com>
2024-07-02 21:05:15 +1000

    typo

--- source/vibe/http/server.d
+++ source/vibe/http/server.d
@@ -992,7 +992,7 @@ final class HTTPServerRequest : HTTPRequest {
 		return m_cookies.get;
 	}
 
-	@property ref const CookieValueMap cookies() cons
+	@property ref const CookieValueMap cookies() const
 	{
 		return m_cookies.get;
 	}
