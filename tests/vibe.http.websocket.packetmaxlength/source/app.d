6d0221e3a3979298c408f3242e6d70bbaa98a25c
Sönke Ludwig <sludwig@outerproduct.org>
2026-03-02 18:17:55 +0100

    Improve web socket test case.
    - Cleans up listrening socket to avoid FD leak messages
    - Introduces a test timeout to avoid hanging the CI indefinitely
    - Uses explicit main()

--- tests/vibe.http.websocket.packetmaxlength/source/app.d
+++ tests/vibe.http.websocket.packetmaxlength/source/app.d
@@ -3,39 +3,39 @@ import vibe.core.log;
 import vibe.inet.url;
 import vibe.http.server;
 import vibe.http.websockets;
+import core.time : seconds;
 
-shared static this()
+
+void main()
 {
+	auto tm = setTimer(10.seconds, { assert(false, "Test timed out."); });
+
 	//test max payload
 	auto settings = new HTTPServerSettings;
 	settings.port = 0;
 	settings.bindAddresses = ["127.0.0.1"];
 	settings.webSocketPayloadMaxLength = 10;
-	immutable serverAddr = listenHTTP(settings, handleWebSockets((scope ws) {
+	auto listener = listenHTTP(settings, handleWebSockets((scope ws) {
 		assert(ws.connected); // issue #2104
 		assert(ws.receiveText() == "1234567890");
 		ws.send("ok");
 		try{
 			ws.receiveText();	//at this point the connection should close
 			assert(false);
-		}catch(Exception e)
-		{
-		}
+		} catch(Exception e) {} // expected
 		ws.close();
-	})).bindAddresses[0];
+	}));
 
-	runTask({
-		scope(exit) exitEventLoop(true);
-
-		try connectWebSocket(URL("http://" ~ serverAddr.toString), (scope ws) {
-			assert(ws.connected);
-			ws.send("1234567890");
-			assert(ws.receiveText() == "ok");
-			ws.send("1234567890a");		//should cause connection to close
-			assert(!ws.waitForData);
-			ws.close();
-			logInfo("WebSocket max payload test successful");
-		});
-		catch (Exception e) assert(false, e.msg);
+	try connectWebSocket(URL("http://" ~ listener.bindAddresses[0].toString), (scope ws) {
+		assert(ws.connected);
+		ws.send("1234567890");
+		assert(ws.receiveText() == "ok");
+		ws.send("1234567890a");		//should cause connection to close
+		assert(!ws.waitForData);
+		ws.close();
+		logInfo("WebSocket max payload test successful");
 	});
+	catch (Exception e) assert(false, e.msg);
+
+	listener.stopListening();
 }
