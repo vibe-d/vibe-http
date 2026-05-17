c55b2b1a891f21f34fc60624bd3d242ae695870b
Alexibu <alex@sunopti.com>
2026-05-18 02:29:45 +1000

    Ensure chunk size is at least 1 when reading data

--- source/vibe/http/proxy.d
+++ source/vibe/http/proxy.d
@@ -87,7 +87,7 @@ private void pump(Src, Dst)(Src src, Dst dst, bool *finished)
 		{
 		case WaitForDataStatus.dataAvailable:
 			auto chunk = min(src.leastSize, buf.length);
-			if (chunk == 0) continue;
+			if (chunk == 0) chunk = 1;
 			src.read(buf[0 .. chunk]);
 			dst.write(buf[0 .. chunk]);
 			break;
